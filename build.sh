cc3baa2d29b3402f85d7b25e650512ef54616d6b
fusedmfg <trboyden@fusedmfg.com>
2026-02-16 09:36:33 -0500

    Add a clean step to the build.sh script

--- build.sh
+++ build.sh
@@ -7,6 +7,10 @@ echo "=== Pulling latest changes ==="
 git pull origin develop
 
 echo ""
+echo "=== Clean the project directory ==="
+dotnet clean
+
+echo ""
 echo "=== Building Release configuration ==="
 dotnet build --configuration Release
 
